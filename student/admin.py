2638ea4c08516a3ca51397bad91647329c38a4a9
mykucukkara <mykucukkara@gmail.com>
2020-12-12 20:53:04 +0300

    admin panel configured

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from student.models import Classroom
+
 
 # Register your models here.
+
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+admin.site.register(Classroom, ClassroomAdmin)
